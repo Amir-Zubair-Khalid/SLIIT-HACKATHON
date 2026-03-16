27dd389a9f937148395dc472d2cece5dcfcca24b
Amir-Zubair-Khalid <amirzubair24@gmail.com>
2026-03-16 17:15:50 +0530

    Add files via upload

---
+++ hackthon web project/hackthon web project/func.js
@@ -0,0 +1,15 @@
+function Notes() {
+  window.location.href = "secondary%20pages/lecture%20notes/lecture.html"
+}
+function Time() {
+  window.location.href = "secondary%20pages/timetable/timetable.html"
+}
+function Guides() {
+  window.location.href = "secondary%20pages/guides/guides.html"
+}
+function Events() {
+  window.location.href = "secondary%20pages/events/events.html"
+}
+function Home() {
+  window.location.href = "../../index.html" // ← for use inside secondary pages
+}
---
+++ hackthon web project/hackthon web project/secondary pages/nav.js
@@ -0,0 +1,17 @@
+// secondary pages/nav.js
+
+function Home() {
+  window.location.href = "../../index.html"
+}
+function Notes() {
+  window.location.href = "../lecture%20notes/lecture.html"
+}
+function Time() {
+  window.location.href = "../timetable/timetable.html"
+}
+function Guides() {
+  window.location.href = "../guides/guides.html"
+}
+function Events() {
+  window.location.href = "../events/events.html"
+}
